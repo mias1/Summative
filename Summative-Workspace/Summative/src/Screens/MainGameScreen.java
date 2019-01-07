5447f670e35fb87e4b5514c58be9e1a5c9d9a22e
User <User@192.168.2.74>
2019-01-07 21:20:38 -0200

    Move Mechanics have been placed in a separate class.

--- Summative-Workspace/Summative/src/Screens/MainGameScreen.java
+++ Summative-Workspace/Summative/src/Screens/MainGameScreen.java
@@ -10,17 +10,16 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 import Game.SonicGame;
 import Launchers.Launcher;
+import Tools.MoveMechanics;
 
-public class MainGameScreen implements Screen {
+public class MainGameScreen extends MoveMechanics implements Screen {
 
-	public static final float SPEED = 120;
-	
 	public static final float CHARACTER_ANIMATION_SPEED = 0.089f;
 	public static final int CHARACTER_WIDTH = 55;
 	public static final int CHARACTER_HEIGHT = 60;
 	
-	float x;
-	float characterY;
+	private static int score = 0;
+	
 	float stateTime;
 	
 	int xBackground1;
@@ -28,6 +27,9 @@ public class MainGameScreen implements Screen {
 	int xBackground2;
 	int xLimit;
 	int backgroundSpeed;
+	int xFloor1;
+	int xFloor2;
+	int floorSpeed;
 	
 	SonicGame game;
 	
@@ -35,22 +37,20 @@ public class MainGameScreen implements Screen {
 	Texture greenHillZoneFloor = new Texture("Summative-Workspace/Summative/assets/plainGreenHillZoneFloor.png");
 	Animation[] run; 
 	
-	String jumpState = "Ground";
-	float jumpSpeed = 15;
-	float gravity = 0.75f;
-	int ground;
-	
 	public MainGameScreen(SonicGame game, String characterSelected) {
 		this.game = game;
-		ground = 30;
-		characterY = 41;
-		x = 10;
+		
+		setMechanics(10, 48, 120, 15, 0.75f, "Ground", 41);
 		
 		xBackground1 = 0;
 		xBackground2 = Launcher.WINDOW_WIDTH;
 		xStart =  xBackground2;
 		xLimit = Launcher.WINDOW_WIDTH * -1;
 		backgroundSpeed = 3;
+		
+		xFloor1 = xBackground1;
+		xFloor2 = xBackground2;
+		floorSpeed = 5;
 		
 		run = new Animation[1];
 		
@@ -89,40 +89,17 @@ public class MainGameScreen implements Screen {
 
 	@Override
 	public void render(float delta) {
-		//System.out.println(jumpState);
-		if(jumpState.equalsIgnoreCase("Descending")) {
-			characterY -= jumpSpeed;
-			jumpSpeed += gravity;
-			
-			if(jumpSpeed >= 15) {
-				jumpState = "Ground";
-				characterY = 41;
-				jumpSpeed = 15;
-			}
-		} else if(jumpState.equalsIgnoreCase("Ascending")) {
-			characterY += jumpSpeed;
-			jumpSpeed -= gravity;
-			
-			if(jumpSpeed <= 0) {
-				jumpState = "Descending";
-				jumpSpeed = 0;
-			}
-		}
+		
+		verifyIfJumping();
 		
 		if(Gdx.input.isKeyPressed(Keys.UP) && jumpState.equalsIgnoreCase("Ground")) {
-			jumpState = "Ascending";
-			characterY += jumpSpeed;
-			jumpSpeed -= gravity;
+			initiateJump();
 		}
 		if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
-			if(x <= 100) {
-				x += SPEED * Gdx.graphics.getDeltaTime();
-			}
+			moveRight();
 		}
 		if(Gdx.input.isKeyPressed(Keys.LEFT)) {
-			if(x >= 0) {
-				x -= SPEED * Gdx.graphics.getDeltaTime();
-			}
+			moveLeft();
 		}
 		
 		stateTime += delta;
@@ -134,8 +111,8 @@ public class MainGameScreen implements Screen {
 		game.batch.draw(skyBackground, xBackground1, 0, Launcher.WINDOW_WIDTH, Launcher.WINDOW_HEIGHT);
 		game.batch.draw(skyBackground, xBackground2, 0, Launcher.WINDOW_WIDTH, Launcher.WINDOW_HEIGHT);
 		
-		game.batch.draw(greenHillZoneFloor, xBackground1, ground - 35, Launcher.WINDOW_WIDTH, 50);
-		game.batch.draw(greenHillZoneFloor, xBackground2, ground - 35, Launcher.WINDOW_WIDTH, 50);
+		game.batch.draw(greenHillZoneFloor, xFloor1, 0, Launcher.WINDOW_WIDTH, 50);
+		game.batch.draw(greenHillZoneFloor, xFloor2, 0, Launcher.WINDOW_WIDTH, 50);
 		
 		if(xBackground1 <= xLimit) {
 			xBackground1 = xStart;
@@ -143,11 +120,19 @@ public class MainGameScreen implements Screen {
 		if(xBackground2 <= xLimit) {
 			xBackground2 = xStart;
 		}
+		if(xFloor1 <= xLimit) {
+			xFloor1 = xStart;
+		}
+		if(xFloor2 <= xLimit) {
+			xFloor2 = xStart;
+		}
 		
 		xBackground1 -= backgroundSpeed;
 		xBackground2 -= backgroundSpeed;
+		xFloor1 -= floorSpeed;
+		xFloor2 -= floorSpeed;
 		
-		game.batch.draw(run[0].getKeyFrame(stateTime, true), x, characterY, CHARACTER_WIDTH, CHARACTER_HEIGHT);
+		game.batch.draw(run[0].getKeyFrame(stateTime, true), actorX, actorY, CHARACTER_WIDTH, CHARACTER_HEIGHT);
 		
 		game.batch.end();
 	}
@@ -166,5 +151,5 @@ public class MainGameScreen implements Screen {
 	public void show() {
 		
 	}
-
+	
 }
---
+++ Summative-Workspace/Summative/src/Tools/MoveMechanics.java
@@ -0,0 +1,144 @@
+package Tools;
+
+import com.badlogic.gdx.Gdx;
+
+/**
+ * @author User
+ *
+ */
+public class MoveMechanics {
+
+	public int actorX;
+	public int actorY;
+	public float speed;
+	private float initialJumpSpeed;
+	public float jumpSpeed;
+	public float gravity;
+	public String jumpState;
+	public int groundLevel;
+	
+	/**
+	 * Defines all the physics components.
+	 * @param actorX
+	 * @param actorY
+	 * @param speed
+	 * @param jumpSpeed
+	 * @param gravity
+	 * @param jumpState
+	 * @param groundLevel
+	 */
+	public void setMechanics(int actorX, int actorY, float speed, float jumpSpeed, float gravity, String jumpState, int groundLevel) {
+		this.actorX = actorX;
+		this.actorY = actorY;
+		this.speed = speed;
+		this.jumpSpeed = jumpSpeed;
+		initialJumpSpeed = jumpSpeed;
+		this.gravity = gravity;
+		this.jumpState = jumpState;
+		this.groundLevel = groundLevel;
+	}
+	
+	/**
+	 * Alters the X-Component of an actor, moving it to the right.
+	 */
+	public void moveRight() {
+		if(actorX <= 100) {
+			if(actorIsInAir()) {
+				actorX += (speed - 60) * Gdx.graphics.getDeltaTime();
+			}else {
+				actorX += speed * Gdx.graphics.getDeltaTime();
+			}
+		}
+	}
+	
+	/**
+	 * Alters the X-Component of an actor, moving it to the left.
+	 */
+	public void moveLeft() {
+		if(actorX >= 0) {
+			if(actorIsInAir()) {
+				actorX -= (speed - 60) * Gdx.graphics.getDeltaTime();
+			}else {
+				actorX -= speed * Gdx.graphics.getDeltaTime();
+			}
+		}
+	}
+	
+	/**
+	 * Verifies if the actor is in the air, if it is, the process of ascending or descending continues.
+	 */
+	public void verifyIfJumping(){
+		if(actorIsInAir()) {
+			verifyStateOfJump();
+		}
+	}
+	
+	/*public void jump() {
+		if(!actorIsInAir()) {
+			initiateJump();
+		}else {
+			continueJump();
+		}
+	}*/
+	
+	/**
+	 * Starts the jump process of an actor.
+	 */
+	public void initiateJump() {
+		jumpState = "Ascending";
+		actorY += jumpSpeed;
+		jumpSpeed -= gravity;
+	}
+	
+	/**
+	 * Checks if the actor is ascending or descending then continues either of the processes.
+	 */
+	private void verifyStateOfJump() {
+		if(jumpState.equalsIgnoreCase("Ascending")) {
+			ascend();
+		} else if(jumpState.equalsIgnoreCase("Descending")) {
+			descend();
+		}
+	}
+	
+	/**
+	 * Makes the actor ascend.
+	 */
+	private void ascend() {
+		actorY += jumpSpeed;
+		jumpSpeed -= gravity;
+		
+		if(jumpSpeed <= 0) {
+			jumpState = "Descending";
+			jumpSpeed = 0;
+		}
+	}
+	
+	/**
+	 * Makes the actor descend.
+	 */
+	private void descend() {
+		actorY -= jumpSpeed;
+		jumpSpeed += gravity;
+		
+		if(jumpSpeed >= initialJumpSpeed) {
+			jumpState = "Ground";
+			actorY = groundLevel;
+			jumpSpeed = initialJumpSpeed;
+		}
+	}
+	
+	
+	/**
+	 * Verifies if the actor is in the air.
+	 * @return true if the actor is in the air or false if it isn't.
+	 */
+	private boolean actorIsInAir() {
+		if(jumpState.equalsIgnoreCase("Ground")) {
+			return false;
+		}else {
+			return true;
+		}
+	}
+	
+}
